64a5ecee955adb65c689083233f214fceb26ddb7
Nina Buchina <m.e.tigra@gmail.com>
2017-01-08 14:22:14 +0100

    Initial: adds Morty sometimes

---
+++ rick.py
@@ -0,0 +1,66 @@
+import random
+
+def rickify(text):
+    new_text=""
+    sentences=break_into_sentences(text)
+    for s in sentences:
+        s=apply_morty_rule(s)
+        s=apply_burp_rule(s)
+        new_text=new_text+s+" "
+    
+    return new_text
+
+def apply_morty_rule(sentence):
+    morty_chance=.05
+    
+    sentence_stoppers=('.','!','?')
+    if not sentence.endswith(sentence_stoppers):
+        return sentence
+        
+    dice=random.random()
+    if dice<=morty_chance:
+        insert_index= next((i for i, ch  in enumerate(sentence) if ch in sentence_stoppers),None)
+        new_sentence=sentence[0:insert_index]+", Morty"+sentence[insert_index:]
+        return new_sentence
+    return sentence
+    
+def apply_burp_rule(sentence):
+    burp_chance=.4
+    return sentence
+
+def break_into_sentences(text):
+    import re
+    caps = "([A-Z])"
+    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+    websites = "[.](com|net|org|io|gov)"
+    
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + caps + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(caps + "[.]" + caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(caps + "[.]" + caps + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + caps + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+
+print (rickify("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."))
